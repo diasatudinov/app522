a96581aa5bb35cc28d1f757d118f6b156a7b6a3b
Dias <dias.atudinov@gmail.com>
2024-09-05 22:25:02 +0500

    user onboarding

--- app522/app522/LoaderUIView/LoaderUIView.swift
+++ app522/app522/LoaderUIView/LoaderUIView.swift
@@ -53,7 +53,7 @@ struct LoaderUIView: View {
                 ReOnboardingUIView()
                 
             } else {
-               // UsOnboardingUIView()
+                UsOnboardingUIView()
             }
             
         }
