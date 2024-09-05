b1274f92c086c2a06d77fce6caf8447e349406ed
Dias <dias.atudinov@gmail.com>
2024-09-05 22:19:41 +0500

    Loading View, Onboardings part 2

---
+++ app522/app522/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  app522
+//
+//  Created by Dias Atudinov on 05.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
