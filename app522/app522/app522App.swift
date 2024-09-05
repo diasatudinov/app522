0352ab20351a6c62764ba1868376062ee0181720
Dias <dias.atudinov@gmail.com>
2024-09-05 22:20:28 +0500

    Loading View, Onboardings part 5

---
+++ app522/app522/app522App.swift
@@ -0,0 +1,17 @@
+//
+//  app522App.swift
+//  app522
+//
+//  Created by Dias Atudinov on 05.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct app522App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
