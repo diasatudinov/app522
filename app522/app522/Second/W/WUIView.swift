35afda9857fbe1142c28a73e14cdf5e9fbbc58e3
Dias <dias.atudinov@gmail.com>
2024-09-18 22:48:42 +0500

    new appIcon and logo

---
+++ app522/app522/Second/W/WUIView.swift
@@ -0,0 +1,18 @@
+//
+//  WUIView.swift
+//  app522
+//
+//  Created by Dias Atudinov on 18.09.2024.
+//
+
+import SwiftUI
+
+struct WUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WUIView()
+}
